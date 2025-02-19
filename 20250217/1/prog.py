8dbd30235fed26452f2e2c7ad40f4ff19179918f
ilia-ilin <ilia.ilin.k@gmail.com>
2025-02-19 15:25:51 +0300

    add feature show_commit

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import zlib
 
 def list_branches(repo_path):
     git_dir = Path(repo_path) / ".git"
@@ -10,6 +11,22 @@ def list_branches(repo_path):
     for branch in heads_dir.glob("*"):
         print(branch.name)
 
+def show_commit(repo_path, branch):
+    git_dir = Path(repo_path) / ".git"
+    ref_path = git_dir / "refs/heads" / branch
+    if not ref_path.exists():
+        raise ValueError(f"Branch {branch} not found")
+    
+    with open(ref_path, "r") as f:
+        commit_sha = f.read().strip()
+    
+    obj_path = git_dir / "objects" / commit_sha[:2] / commit_sha[2:]
+    with open(obj_path, "rb") as f:
+        raw = zlib.decompress(f.read())
+    
+    header, _, body = raw.partition(b'\x00')
+    print(body.decode())
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: prog.py REPO_PATH [BRANCH]")
@@ -18,3 +35,5 @@ if __name__ == "__main__":
     repo_path = sys.argv[1]
     if len(sys.argv) == 2:
         list_branches(repo_path)
+    elif len(sys.argv) == 3:
+        show_commit(sys.argv[1], sys.argv[2])
